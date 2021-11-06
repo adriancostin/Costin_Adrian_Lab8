a81e7ac8da523540e6fd1d1d02f162b8f2f8f1fa
Adrian Costin <adrian.costin@bitnet.info>
2021-11-06 15:48:56 +0200

    Add project files.

---
+++ Data/Costin_Adrian_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Costin_Adrian_Lab8.Models;
+
+namespace Costin_Adrian_Lab8.Data
+{
+    public class Costin_Adrian_Lab8Context : DbContext
+    {
+        public Costin_Adrian_Lab8Context (DbContextOptions<Costin_Adrian_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Costin_Adrian_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Costin_Adrian_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Costin_Adrian_Lab8.Models.Category> Category { get; set; }
+    }
+}
